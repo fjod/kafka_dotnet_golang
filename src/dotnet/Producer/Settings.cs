c158bb39ae70267a5250d07cf3239a9d34538501
Michael <fourjustoneday@yandex.ru>
2023-05-30 09:25:42 +0300

    .net producer

---
+++ src/dotnet/Producer/Settings.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Producer;
+
+public static class Settings
+{
+    public static IConfiguration Get()
+    {
+        var exePath = System.Reflection.Assembly.GetEntryAssembly()?.Location;
+        if (exePath == null) throw new Exception("Cant find current location");
+        var basePath = Path.GetDirectoryName(exePath);
+        if (basePath == null) throw new Exception("Cant create base path for settings");
+        var propPath = Path.Combine(basePath, "producer.properties");
+        return new ConfigurationBuilder()
+            .AddIniFile(propPath)
+            .Build();
+    } 
+}
